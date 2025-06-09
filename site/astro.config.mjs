78920b2727fccb4e16233f8230306ffa6b4474a5
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-06-09 19:50:29 +0500

    fix code formatting

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,18 +3,18 @@ import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-      resolve: {
-          alias: {
-              '@': fileURLToPath(new URL('./src', import.meta.url)),
-          },
-      },
-  },
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+    },
 
-  integrations: [react()],
+    integrations: [react()],
 })
