54ffade532eae57e2d53778b38e423a928b0e4d7
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-06-09 16:02:47 +0500

    install shadcn and tailwind

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              '@': fileURLToPath(new URL('./src', import.meta.url)),
+          },
+      },
+  },
+
+  integrations: [react()],
+})
