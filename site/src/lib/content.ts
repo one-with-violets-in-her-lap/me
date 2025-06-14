73619664cfa7e75790a8bd6d1d1f519bd1a3f1b7
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-06-14 18:56:47 +0500

    site/content: add images

--- site/src/lib/content.ts
+++ site/src/lib/content.ts
@@ -1,18 +1,27 @@
 import { strapi, type API } from '@strapi/client'
+import type { StrapiImage } from '@/lib/strapi-utils'
 
 export interface Post extends API.Document {
     content: string
+    photo?: Photo
+}
+
+export interface Photo extends API.Document {
+    name: string
+    description: string
+    takenDate: string
+    image: StrapiImage
 }
 
 const strapiClient = strapi({
-    baseURL: import.meta.env.PUBLIC_STRAPI_API_BASE_URL,
+    baseURL: import.meta.env.PUBLIC_STRAPI_CMS_BASE_URL + '/api',
     auth: import.meta.env.PUBLIC_STRAPI_API_TOKEN,
 })
 
 export const contentApiClient = {
     async fetchPosts() {
-        return (await strapiClient
-            .collection('posts')
-            .find()) as API.DocumentResponseCollection<Post>
+        return (await strapiClient.collection('posts').find({
+            populate: 'photo.image',
+        })) as API.DocumentResponseCollection<Post>
     },
 }
---
+++ site/src/lib/strapi-utils.ts
@@ -0,0 +1,37 @@
+import type { API } from '@strapi/client'
+
+export interface StrapiImage extends API.Document {
+    alternativeText: string | null
+    caption: string | null
+    width: number
+    height: number
+    formats: {
+        thumbnail: {
+            width: number
+            height: number
+            size: number
+            url: string
+        }
+        small: {
+            width: number
+            height: number
+            size: number
+            url: string
+        }
+        large: {
+            width: number
+            height: number
+            size: number
+            url: string
+        }
+        medium: {
+            width: number
+            height: number
+            size: number
+            url: string
+        }
+    }
+
+    size: number
+    url: string
+}
