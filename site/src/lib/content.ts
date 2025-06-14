484636b456ccac721392fc24158cdbbe153dec10
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-06-14 20:13:12 +0500

    add basic song card

--- site/src/lib/content.ts
+++ site/src/lib/content.ts
@@ -1,16 +1,26 @@
 import { strapi, type API } from '@strapi/client'
-import type { StrapiImage } from '@/lib/strapi-utils'
+import type { StrapiDocument, StrapiImage, StrapiMedia } from '@/lib/strapi-utils'
 
-export interface Post extends API.Document {
+export interface Post extends StrapiDocument {
     content: string
-    photo?: Photo
+    photo: Photo | null
+    song: Song | null
 }
 
-export interface Photo extends API.Document {
+export interface Photo extends StrapiDocument {
     name: string
     description: string
     takenDate: string
     image: StrapiImage
+}
+
+export interface Song extends StrapiDocument {
+    name: string
+    description: string
+    secondsDuration: number
+    bitrateKbps: number
+    audioFile: StrapiMedia
+    thumbnail: StrapiImage
 }
 
 const strapiClient = strapi({
@@ -21,7 +31,7 @@ const strapiClient = strapi({
 export const contentApiClient = {
     async fetchPosts() {
         return (await strapiClient.collection('posts').find({
-            populate: 'photo.image',
+            populate: ['photo.image', 'song.audioFile', 'song.thumbnail'],
         })) as API.DocumentResponseCollection<Post>
     },
 }
--- site/src/lib/strapi-utils.ts
+++ site/src/lib/strapi-utils.ts
@@ -1,10 +1,20 @@
-import type { API } from '@strapi/client'
+export interface StrapiDocument {
+    documentId: string
+    createdAt: string
+    updatedAt: string
+}
 
-export interface StrapiImage {
+export interface StrapiMedia extends StrapiDocument {
     alternativeText: string | null
     caption: string | null
     width: number
     height: number
+
+    size: number
+    url: string
+}
+
+export interface StrapiImage extends StrapiMedia {
     formats: {
         thumbnail: {
             width: number
@@ -31,7 +41,4 @@ export interface StrapiImage {
             url: string
         }
     }
-
-    size: number
-    url: string
 }
