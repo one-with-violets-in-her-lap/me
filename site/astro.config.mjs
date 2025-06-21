219f2a003410cb5833eb0f9c85cd3feb1f677dbe
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-06-21 17:01:11 +0500

    site: install netlify adapter

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,21 +7,25 @@ import react from '@astrojs/react'
 
 import { customRouting } from '@inox-tools/custom-routing'
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url)),
-            },
-        },
-    },
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              '@': fileURLToPath(new URL('./src', import.meta.url)),
+          },
+      },
+  },
 
-    integrations: [
-        react(),
-        customRouting({
-            '/': './src/pages/home/page.astro',
-        }),
-    ],
+  integrations: [
+      react(),
+      customRouting({
+          '/': './src/pages/home/page.astro',
+      }),
+  ],
+
+  adapter: netlify(),
 })
