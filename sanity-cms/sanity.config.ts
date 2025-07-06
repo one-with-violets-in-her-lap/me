6ebde181c5a6629032412872a305a101b71a6936
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-07-06 19:40:27 +0500

    cms: fix code formatting

--- sanity-cms/sanity.config.ts
+++ sanity-cms/sanity.config.ts
@@ -4,7 +4,10 @@ import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemaTypes'
 import { markdownSchema } from 'sanity-plugin-markdown'
 
-if (!process.env.SANITY_STUDIO_PROJECT_ID || !process.env.SANITY_STUDIO_DATASET) {
+if (
+    !process.env.SANITY_STUDIO_PROJECT_ID ||
+    !process.env.SANITY_STUDIO_DATASET
+) {
     throw new Error(
         'Env vars SANITY_STUDIO_PROJECT_ID and SANITY_STUDIO_DATASET must be specified',
     )
